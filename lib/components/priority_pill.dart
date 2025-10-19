089b5e9909e6a357f980fa83a7a979eb1e6b677e
sophiehogg <sophiehogg10@gmail.com>
2025-10-19 11:14:08 +1100

    add priority pill - missed in previous push

---
+++ lib/components/priority_pill.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todos/helpers/text_helpers.dart';
+
+import '../data/database.dart';
+
+class PriorityPill extends StatelessWidget {
+  const PriorityPill({super.key, required this.priority});
+  final Priority priority;
+
+  final Map<Priority, Color> priorityColours = const {
+    Priority.high: Colors.red,
+    Priority.medium: Colors.orange,
+    Priority.low: Colors.green,
+  };
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: priorityColours[priority], borderRadius: BorderRadius.circular(20)),
+        padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+        child: Text(priority.name.toSentenceCase(), style: TextStyle(color: Colors.white, fontSize: 12))
+    );
+  }
+}
