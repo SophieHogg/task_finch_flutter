c4671d23fdc4450a2acfcd1fea5fe139f47fbc2b
sophiehogg <sophiehogg10@gmail.com>
2025-10-25 01:10:38 +1100

    priority selector adjustment

--- lib/components/priority_selector.dart
+++ lib/components/priority_selector.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:task_finch/components/priority_circle.dart';
 import 'package:task_finch/helpers/text_helpers.dart';
 import 'package:task_finch/theming/business_logic_theming.dart';
-import 'package:collection/collection.dart';
 import '../data/database.dart';
 
 typedef PriorityEntry = DropdownMenuEntry<Priority>;
@@ -17,13 +15,13 @@ class PrioritySelector extends HookWidget {
 
   final Priority priority;
   final void Function(Priority priority) onChangePriority;
+
   // ease out back jumps past the edges too far
   // this adjusts the default values: 'd' from 1.275 to 1.10
   static const _betterEaseOutBack = Cubic(0.175, 0.885, 0.32, 1.10);
 
   @override
   Widget build(BuildContext context) {
-    final _lastSelection = useState<Priority>(priority);
     return Container(
       height: 40,
       decoration: BoxDecoration(
@@ -39,75 +37,54 @@ class PrioritySelector extends HookWidget {
               duration: Duration(milliseconds: 300),
               curve: _betterEaseOutBack,
               child: FractionallySizedBox(
-                widthFactor: 1/3,
-                child: Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.black.withAlpha(40),
-                    borderRadius: BorderRadius.circular(100),
-                    border: Border.all(color: Colors.white, width: 3),
+                widthFactor: 1 / 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black.withAlpha(40),
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.white, width: 2),
+                    ),
                   ),
                 ),
               ),
             ),
             Row(
               children:
-                  [
-                    for (final priorityGradient in priorityGradients.entries)
-                      Expanded(
-                        child: InkWell(
-                          splashColor: Colors.white.withAlpha(50),
-                          borderRadius: BorderRadius.circular(100),
-                          onTap:
-                              () => this.onChangePriority(priorityGradient.key),
-                          child: Center(
-                            child: AnimatedDefaultTextStyle(
-                              style: TextStyle(
-                                color:
-                                priorityGradient.key == priority
-                                    ? Colors.white
-                                    : Colors.black,
-                              ),
-                              duration: Duration(milliseconds: 200),
-                              child: Text(
-                                priorityGradient.key.name.toSentenceCase(),
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
+              [
+                for (final priorityGradient in priorityGradients.entries)
+                  Expanded(
+                    child: InkWell(
+                      splashColor: Colors.white.withAlpha(50),
+                      borderRadius: BorderRadius.circular(100),
+                      onTap:
+                          () => this.onChangePriority(priorityGradient.key),
+                      child: Center(
+                        child: AnimatedDefaultTextStyle(
+                          style: TextStyle(
+                            color:
+                            priorityGradient.key == priority
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                          duration: Duration(milliseconds: 200),
+                          child: Text(
+                            priorityGradient.key.name.toSentenceCase(),
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
                             ),
                           ),
                         ),
                       ),
-                  ].reversed.toList(),
+                    ),
+                  ),
+              ].reversed.toList(),
             ),
-
           ],
-
         ),
       ),
-    );
-    return DropdownMenu<Priority>(
-      initialSelection: _lastSelection.value,
-      enableSearch: false,
-      requestFocusOnTap: false,
-      onSelected: (Priority? priority) {
-        _lastSelection.value = priority ?? Priority.medium;
-        onChangePriority(priority ?? Priority.medium);
-      },
-      leadingIcon: PriorityCircle(priority: _lastSelection.value),
-      expandedInsets: EdgeInsets.zero,
-      enableFilter: false,
-      inputDecorationTheme: InputDecorationTheme.of(context),
-      dropdownMenuEntries: [
-        for (final priority in priorityGradients.entries)
-          PriorityEntry(
-            value: priority.key,
-            label: priority.key.name.toSentenceCase(),
-            leadingIcon: PriorityCircle(priority: priority.key),
-          ),
-      ],
     );
   }
 }
